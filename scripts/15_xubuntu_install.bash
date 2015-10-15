53a7295fd6fded923f5590d7577df20262a3e806
Andréas Livet <andreas.livet@gmail.com>
2015-10-15 18:26:45 +0200

    Add gnome-font-viewer packag

--- scripts/15_xubuntu_install.bash
+++ scripts/15_xubuntu_install.bash
@@ -39,7 +39,7 @@ echo
 echo "=============================="
 echo "Installing system packages"
 echo "=============================="
-sudo apt-get install -y gnome-system-monitor synaptic gdebi-core gksu rar unrar usb-creator-gtk wget gparted
+sudo apt-get install -y gnome-system-monitor synaptic gdebi-core gksu rar unrar usb-creator-gtk wget gparted gnome-font-viewer
 
 
 echo
