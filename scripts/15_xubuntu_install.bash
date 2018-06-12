cec609d0b5b729efb7dd982c63c58241d5ee60dd
Andréas Livet <andreas.livet@gmail.com>
2018-06-12 15:24:13 +0200

    Update to Xubuntu 18.04

--- scripts/15_xubuntu_install.bash
+++ scripts/15_xubuntu_install.bash
@@ -3,19 +3,17 @@ echo "=============================="
 echo "Removing useless Xubuntu package"
 echo "=============================="
 
-sudo apt-get remove -y ristretto parole apport
+sudo apt-get remove -y ristretto parole xfburn
 
 echo
 echo "=============================="
-echo "Adding Wine, SublimeText, Pinta, Peek, shotcut and nodejs ppa"
+echo "Adding Peek, shotcut and yarn ppa"
 echo "=============================="
-
-sudo add-apt-repository ppa:wine/wine-builds
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
 sudo add-apt-repository ppa:haraldhv/shotcut
 sudo add-apt-repository ppa:peek-developers/stable
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 
 echo
 echo "=============================="
@@ -28,13 +26,14 @@ echo
 echo "=============================="
 echo "Installing good multimedia packages"
 echo "=============================="
-sudo apt-get install -y vlc xubuntu-restricted-extras libavcodec-extra cheese brasero shotwell shotcut sound-juicer ogmrip handbrake libdvd-pkg
+sudo apt-get install -y vlc xubuntu-restricted-extras cheese brasero shotwell shotcut sound-juicer ogmrip handbrake libdvd-pkg
 #Install encrypted dvd library
 sudo dpkg-reconfigure libdvd-pkg
 
 echo
 echo "=============================="
 echo "Installing graphic design packages"
+echo "Or Install gimp from 'Software' with snap"
 echo "=============================="
 sudo apt-get install -y gimp inkscape pinta peek
 
@@ -42,7 +41,7 @@ echo
 echo "=============================="
 echo "Installing system packages"
 echo "=============================="
-sudo apt-get install -y pdftk gnome-system-monitor synaptic gdebi-core gksu rar git curl unrar usb-creator-gtk wget gparted gnome-font-viewer
+sudo apt-get install -y gnome-system-monitor gdebi-core rar git curl unrar usb-creator-gtk wget gparted gnome-font-viewer
 
 
 echo
@@ -59,30 +58,18 @@ sudo apt-get install -y --install-recommends winehq-devel
 
 echo
 echo "=============================="
-echo "Installing Sublime Text 3"
+echo "Installing Sublime Text 3 from 'Software' with snap"
 echo "=============================="
-sudo apt-get install -y sublime-text-installer
 
 echo
 echo "=============================="
 echo "Installing Node.js"
 echo "=============================="
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs yarn
 
 echo
 echo "=============================="
 echo "Installing MySql"
 echo "=============================="
 sudo apt-get install -y mysql-server libmysqld-dev
-
-echo
-echo "=============================="
-echo "Installing Skype"
-echo "=============================="
-
-
-echo
-echo "=============================="
-echo "Installing DropBox"
-echo "=============================="
 
